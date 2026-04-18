c7d8b759cd4bd98f9cc14206ee2bb18e5692a460
Antonio Tolentino <atcjunior@gmail.com>
2026-04-18 11:28:14 -0300

    force quality gate error

--- main.go
+++ main.go
@@ -3,31 +3,10 @@ package main
 import (
 	"books/storage"
 	"books/web"
-	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 )
-
-// FAILURE 3: High Code Duplication
-// Standard gates fail if > 3.0% of lines are duplicated.
-func triggerDuplicationA() {
-	fmt.Println("This block is identical to the one below.")
-	fmt.Println("SonarQube will flag this as a duplication issue.")
-	items := []string{"one", "two", "three", "four", "five"}
-	for _, item := range items {
-		fmt.Printf("Processing item: %s\n", item)
-	}
-}
-
-func triggerDuplicationB() {
-	fmt.Println("This block is identical to the one below.")
-	fmt.Println("SonarQube will flag this as a duplication issue.")
-	items := []string{"one", "two", "three", "four", "five"}
-	for _, item := range items {
-		fmt.Printf("Processing item: %s\n", item)
-	}
-}
 
 // main is the entry point of the books API.
 // It initializes the logger, storage and web server.
@@ -48,12 +27,8 @@ func main() {
 	slog.Info("Server running at http://localhost:8080")
 
 	// Start Server using slog
-	//if err := http.ListenAndServe(":8080", r); err != nil {
-	//	slog.Error(err.Error())
-	//}
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		slog.Error(err.Error())
+	}
 
-	http.ListenAndServe(":8080", r)
-
-	triggerDuplicationA()
-	triggerDuplicationB()
 }
